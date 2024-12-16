593be81210fcafe95d03c731b8fe62f93d4b09ea
coili <coili@disroot.org>
2024-12-16 02:23:32 +0100

    Upload Malheim files

---
+++ malheim.py
@@ -0,0 +1,235 @@
+"""                                                        
+                                                                                        
+                                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣆⣦⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣇⣏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⣻⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                                ⠀⠀⠀⠀⠀⠀⣠⣤⣤⣤⣤⣶⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                                ⠀⠀⠀⠀⣰⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  
+                                ⠀⠀⠀⢰⣻⣿⣿⣿⣿⣿⡟⠻⠟⠛⠋⣀⡀⣀⡀⠀⡀⠀⠀⠀⠀⠀
+                                ⠀⠀⠀⠀⢿⣿⣿⣿⣿⣤⣤⣤⣴⣶⣷⣿⣿⣿⣿⣿⣷⣷⣤⣀⠀⠀  
+                                ⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡥⠀ 
+                                ⠀⠀⠀⠀⠀⠀⠘⣛⣿⣿⣿⣿⠿⠿⠟⠉⠉⠁⠉⠻⢾⣿⣿⣿⣿⠆
+                                ⠀⠀⠀⢀⣤⣶⣾⣿⣿⣿⣿⣷⣿⣦⣆⣤⣀⠀⠀⠀⣺⣿⣿⣿⣿⡃
+                                ⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⢿⠃
+                                ⠀⣼⣿⣿⣿⣿⣿⡿⠿⠟⠉⠿⢿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⠚⠀
+                                ⢰⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠈⠗⣻⣿⣿⣿⣿⡿⠟⠋⠁⠀⠀
+                                ⠸⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⢀⣴⣾⣿⣿⣿⣿⣿⣾⡁⠀⠀⠀⠀⠀
+                                ⠈⢿⣿⣿⣿⣯⣠⣀⣀⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣂⠀⠀⠀⠀
+                                ⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠋⠉⠓⣿⣿⣿⡷⠀⠀⠀⠀
+                                ⠀⠀⠀⠉⠹⠿⣿⣿⣿⣿⣿⣿⠿⠃⠀⠀⣀⣤⣿⣿⣿⣿⠀⠀⠀⠀
+                                ⠀⠀⠀⠀⠀⠀⠈⠛⠛⠛⣥⣶⣷⣾⣿⣿⣿⣿⣿⣿⠿⠃⠀⠀⠀⠀
+                                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⠿⠿⠿⠟⠛⠉⠁⠀⠀⠀⠀⠀⠀
+                                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⢿⣟⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢲⡿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠯⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+
+
+   ██▀███   ██░ ██  ▄▄▄       ▄████▄   ██ ▄█▀ ███▄    █  ▄▄▄       ██▀███   ▒█████   ██ ▄█▀
+  ▓██ ▒ ██▒▓██░ ██▒▒████▄    ▒██▀ ▀█   ██▄█▒  ██ ▀█   █ ▒████▄    ▓██ ▒ ██▒▒██▒  ██▒ ██▄█▒ 
+  ▓██ ░▄█ ▒▒██▀▀██░▒██  ▀█▄  ▒▓█    ▄ ▓███▄░ ▓██  ▀█ ██▒▒██  ▀█▄  ▓██ ░▄█ ▒▒██░  ██▒▓███▄░ 
+  ▒██▀▀█▄  ░▓█ ░██ ░██▄▄▄▄██ ▒▓▓▄ ▄██▒▓██ █▄ ▓██▒  ▐▌██▒░██▄▄▄▄██ ▒██▀▀█▄  ▒██   ██░▓██ █▄ 
+  ░██▓ ▒██▒░▓█▒░██▓ ▓█   ▓██▒▒ ▓███▀ ░▒██▒ █▄▒██░   ▓██░ ▓█   ▓██▒░██▓ ▒██▒░ ████▓▒░▒██▒ █▄
+  ░ ▒▓ ░▒▓░ ▒ ░░▒░▒ ▒▒   ▓▒█░░ ░▒ ▒  ░▒ ▒▒ ▓▒░ ▒░   ▒ ▒  ▒▒   ▓▒█░░ ▒▓ ░▒▓░░ ▒░▒░▒░ ▒ ▒▒ ▓▒
+    ░▒ ░ ▒░ ▒ ░▒░ ░  ▒   ▒▒ ░  ░  ▒   ░ ░▒ ▒░░ ░░   ░ ▒░  ▒   ▒▒ ░  ░▒ ░ ▒░  ░ ▒ ▒░ ░ ░▒ ▒░
+    ░░   ░  ░  ░░ ░  ░   ▒   ░        ░ ░░ ░    ░   ░ ░   ░   ▒     ░░   ░ ░ ░ ░ ▒  ░ ░░ ░ 
+     ░      ░  ░  ░      ░  ░░ ░      ░  ░            ░       ░  ░   ░         ░ ░  ░  ░   
+                             ░                                                             
+
+                             __  __   _   _    _  _ ___ ___ __  __ 
+                            |  \/  | /_\ | |  | || | __|_ _|  \/  |
+                            | |\/| |/ _ \| |__| __ | _| | || |\/| |
+                            |_|  |_/_/ \_\____|_||_|___|___|_|  |_|
+                                                                                                                                                                                                                                                                                                                    
+"""
+
+import base64
+import configparser
+import json
+import logging
+import requests
+import time
+
+from os import listdir
+from os import remove
+from os.path import isfile, join
+
+# Check if there are new files
+def check_new_files(download_dir) -> None:
+    files = [f for f in listdir(download_dir) if isfile(join(download_dir, f))]
+    if len(files) > 0:
+        return files
+    return False
+
+
+# Upload hash or file to VT
+def upload_to_vt(file, download_dir, vt_token, sleep_timing):
+    commit_message = ''
+    malware_type = ''
+    rmt_folder = 'flagged'
+
+    url = f"https://www.virustotal.com/api/v3/search?query={file}"
+
+    headers = {
+        'accept': 'application/json',
+        'x-apikey': vt_token
+    }
+
+    time.sleep(sleep_timing)
+
+    loop = True
+    while loop:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200 or response.status_code == 201:
+            report = response.json()
+            if len(report["data"]) > 0:
+                if report["data"][0]["attributes"]["sha256"] is not None:
+                    if report["data"][0]["attributes"]["last_analysis_stats"]["malicious"] == 0:
+                        rmt_folder = 'unflagged'
+                        commit_message = 'New sample of potential malware uploaded!'
+                    else:
+                        try:
+                            commit_message = report["data"][0]["attributes"]["type_description"] + " - " + report["data"][0]["attributes"]["popular_threat_classification"]["suggested_threat_label"]
+                            malware_type = report["data"][0]["attributes"]["popular_threat_classification"]["suggested_threat_label"]
+                        except:
+                            try:
+                                commit_message = report["data"][0]["attributes"]["type_description"] + " - cannot determine type of malware"
+                            except:
+                                commit_message = 'New malware uploaded! (Cannot determine type of malware)'
+                    loop = False
+                else:
+                    logging.info("VT: Scan not finished yet.")
+                    time.sleep(sleep_timing * 3)
+            else:
+                url_file = "https://www.virustotal.com/api/v3/files"
+                full_path = download_dir + file
+                with open(full_path, 'rb') as f:
+                    vt_files = {'file': (full_path.split("/")[-1], f)}
+                    response_file = requests.post(url_file, headers=headers, files=vt_files)
+
+                if response_file.status_code == 200 or response_file.status_code == 201:
+                    analysis_id = response_file.json()["data"]["id"]
+                    url_file = f"https://www.virustotal.com/api/v3/analyses/{analysis_id}"
+
+                    loop_file = True
+                    while loop_file:
+                        response_file = requests.get(url_file, headers=headers)
+                        if response_file.status_code == 200 or response_file.status_code == 201:
+                            report = response_file.json()
+                            if report["data"]["attributes"]["status"] == "completed":
+                                if report["data"]["attributes"]["stats"]["malicious"] == 0:
+                                    rmt_folder = 'unflagged'
+                                    commit_message = 'New sample of potential malware uploaded!'
+                                else:
+                                    commit_message = 'New sample of malware uploaded!'
+                                loop_file = False
+                            else:
+                                logging.info(f'VT: Status: {report["data"]["attributes"]["status"]}')
+                                time.sleep(sleep_timing * 3)
+                        else:
+                                logging.info(f"VT: Error while getting report! Error: {response_file.json()}") 
+                else:
+                    logging.info(f"VT: Error while uploading file {full_path} - {response_file.json()}")
+        else:
+            logging.info(f"VT: Error while getting report! Error: {response.json()}")
+            time.sleep(sleep_timing)
+    
+    return rmt_folder, commit_message, malware_type
+
+
+# Upload file to Github
+def upload_to_github(file_path, git_repo, git_owner, git_token, vt_result, type_rejected) -> None:
+    remote_path = f'{vt_result[0]}/{file_path.split("/")[-1]}'
+    url = f'https://api.github.com/repos/{git_owner}/{git_repo}/contents/{remote_path}'
+
+    with open(file_path, 'rb') as f:
+        content = base64.b64encode(f.read()).decode('utf-8')
+
+    headers = {'Authorization': f'token {git_token}'}
+    data = {
+            'message': vt_result[1],
+            'content': content,
+    }
+
+
+    if type_rejected not in vt_result[2]:
+        response = requests.put(url, headers=headers, json=data)
+        if response.status_code == 200 or response.status_code == 201:
+            logging.info(f"GitHub: Upload succeeded for file {file_path}!")
+        else:
+            logging.info(f"GitHub: Upload failed! Error: {response.json()}")
+    else:
+        logging.info(f"GitHub: type {type_rejected} was correctly rejected.")
+
+# Execute all upload functions
+def upload(config) -> None:
+    download_dirs = []
+    for honeypots_file in config["honeypots"].items():
+        download_dirs.append(honeypots_file[1])
+
+    for download_dir in download_dirs:
+        files = check_new_files(download_dir=download_dir)
+
+        if files:
+            git_repo = config["github"]["git_repo"]
+            git_owner = config["github"]["git_owner"]
+            git_token = config["github"]["git_token"]
+
+            vt_token = config["virustotal"]["vt_token"]
+
+            uploaded_files_path = config["malwares"]["already_uploaded_file"]
+            type_rejected = config["malwares"]["type_rejected"]
+            remove_file = config["malwares"]["remove_file"]
+
+            sleep_timing = int(config["sleep"]["timing"])
+
+            try:
+                with open(uploaded_files_path, 'r') as f:
+                    uploaded_files = json.load(f)
+            except:
+                pass
+
+            for file in files:
+                file_path = download_dir + file
+                logging.info(f"File to scan: {file_path}")
+
+                if uploaded_files.get(file) is None:
+                    logging.info(f"Uploading file ({file_path}) to VirusTotal...")
+                    vt_result = upload_to_vt(file=file, download_dir=download_dir, vt_token=vt_token, sleep_timing=sleep_timing)
+
+                    logging.info(f"Uploading file ({file_path}) to GitHub...")
+                    upload_to_github(file_path=file_path, git_repo=git_repo, git_owner=git_owner, git_token=git_token, vt_result=vt_result, type_rejected=type_rejected)
+
+                    logging.info(f"Adding file hash ({file}) to already uploaded list...")
+                    uploaded_files[file] = 1
+
+                    logging.info(f"Saving list...")
+                    with open(uploaded_files_path, 'w') as f:
+                        json.dump(uploaded_files, f, ensure_ascii=False, indent=4)
+                    
+                    logging.info(f"Scan of file {file_path} completed!")
+                
+                else:
+                    logging.info(f"File {file} already uploaded.")
+
+                if remove_file == "yes":
+                    remove(file_path)
+                    logging.info(f"File ({file_path}) deleted!")
+        
+        else:
+            logging.info("No new files.")
+
+
+# Main fonction
+if __name__ == "__main__":
+
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+
+    logging_file = config["logging"]["path"]
+    logging.basicConfig(level=logging.INFO, filename=logging_file, format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %I:%M:%S %p')
+
+    upload(config=config)
